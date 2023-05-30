540f0b0439a1d3adebe05a723c92998813b52a8e
pavanikakustam <46951701+pavanikakustam@users.noreply.github.com>
2023-05-30 15:46:19 +0530

    Add files via upload

---
+++ Assignment3/Enum.js
@@ -0,0 +1,9 @@
+var Colour;
+(function (Colour) {
+    Colour[Colour["Red"] = 0] = "Red";
+    Colour[Colour["Green"] = 1] = "Green";
+    Colour[Colour["Blue"] = 2] = "Blue";
+})(Colour || (Colour = {}));
+console.log(Colour[0]);
+console.log(Colour[1]);
+console.log(Colour[2]);
