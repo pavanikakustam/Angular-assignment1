badee0cb615a65a66254bc0b7b157c9b62ee1f04
pavanikakustam <46951701+pavanikakustam@users.noreply.github.com>
2023-05-30 15:45:07 +0530

    Delete Enum.js

--- Enum.js
+++
@@ -1,9 +0,0 @@
-var Colour;
-(function (Colour) {
-    Colour[Colour["Red"] = 0] = "Red";
-    Colour[Colour["Green"] = 1] = "Green";
-    Colour[Colour["Blue"] = 2] = "Blue";
-})(Colour || (Colour = {}));
-console.log(Colour[0]);
-console.log(Colour[1]);
-console.log(Colour[2]);
