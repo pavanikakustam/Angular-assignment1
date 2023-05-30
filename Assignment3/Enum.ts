c4a7e10c6c4dc36f0c97cfd0768776de9d1bdf94
pavanikakustam <46951701+pavanikakustam@users.noreply.github.com>
2023-05-30 15:38:48 +0530

    Update Enum.ts

--- Assignment3/Enum.ts
+++ Assignment3/Enum.ts
@@ -4,6 +4,6 @@ enum Colour {
     Blue,
   }
   
-  console.log(Colour[0]); // Output: 0
-  console.log(Colour[1]); // Output: 1
-  console.log(Colour[2]); // Output: 2
+  console.log(Colour[0]); 
+  console.log(Colour[1]); 
+  console.log(Colour[2]); 
