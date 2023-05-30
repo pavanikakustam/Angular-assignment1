7adcc5b9498915d9e81e0ebedef6dbee6a26a79f
pavanikakustam <46951701+pavanikakustam@users.noreply.github.com>
2023-05-30 15:39:42 +0530

    Update Enum.js

--- Assignment3/Enum.js
+++ Assignment3/Enum.js
@@ -4,6 +4,6 @@ var Colour;
     Colour[Colour["Green"] = 1] = "Green";
     Colour[Colour["Blue"] = 2] = "Blue";
 })(Colour || (Colour = {}));
-console.log(Colour[0]); // Output: 0
-console.log(Colour[1]); // Output: 1
-console.log(Colour[2]); // Output: 2
+console.log(Colour[0]);
+console.log(Colour[1]);
+console.log(Colour[2]); 
